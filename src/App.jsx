904db2ee939f5e161cc96228ca41998b0d69dac6
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-10-02 21:29:48 +0200

    fixat och importerat states, och propdrillat ner för första gången till komponent från eget complib! baaalllttt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 import { Login } from "@fredwe/frk-1-components";
 
 function App() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const [happyMessage, setHappyMessage] = useState("");
+
+  const loginHandler = () => {
+    setHappyMessage("IT WOOOORKS!!!!!");
+  };
+
   return (
     <>
       <div>
         <h1>Hello World</h1>
-        <Login />
+        <Login
+          setUser={setUser}
+          setPassword={setPassword}
+          loginHandler={loginHandler}
+          buttonText="Logga in"
+        />
+        {user}
+        {password}
+        <h3>{happyMessage || "undrar om det fungerar?"}</h3>
       </div>
     </>
   );
