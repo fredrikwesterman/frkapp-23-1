7e74e82f9f81b67a5261395731c348c33ad4d6f0
Fredrik Westerman <fredrikwesterman9@gmail.com>
2024-10-07 21:34:44 +0200

    fixade funktion för att renderera produkt baserat på värde i state med samma knapp

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
 import "./App.css";
 import { useState } from "react";
-import { Login } from "@fredwe/frk-1-components";
+import { Login, Navbar } from "@fredwe/frk-1-components";
+import Apple from "./Apple";
+import Pear from "./Pear";
+import Banana from "./Banana";
 
 function App() {
   const [user, setUser] = useState("");
   const [password, setPassword] = useState("");
   const [happyMessage, setHappyMessage] = useState("");
+  const [renderedComponent, setRenderedComponent] = useState(null);
+  const [category, setCategory] = useState(null);
 
   const loginHandler = () => {
     setHappyMessage("IT WOOOORKS!!!!!");
   };
 
+  const categorySetter = (title) => {
+    setCategory(title);
+  };
+
+  const renderComponent = (title) => {
+    switch (title) {
+      case "äpple":
+        setRenderedComponent(<Apple />);
+        break;
+      case "päron":
+        setRenderedComponent(<Pear />);
+        break;
+      case "banan":
+        setRenderedComponent(<Banana />);
+        break;
+      default:
+        setRenderedComponent(null);
+    }
+  };
+
   return (
     <>
+      <Navbar renderComponent={() => renderComponent(category)} />
+      <button onClick={() => categorySetter("äpple")}>äpple</button>
+      <button onClick={() => categorySetter("päron")}>päron</button>
+      <button onClick={() => categorySetter("banan")}>banan</button>
+      {category}
       <div>
+        <div>{renderedComponent}</div>
         <h1>Hello World</h1>
         <Login
           setUser={setUser}
---
+++ src/Pear.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Pear = () => {
+  return <div>Pear</div>;
+};
+
+export default Pear;
